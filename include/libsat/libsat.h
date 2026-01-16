b05a0e4b9c2496220a981dd40f38fa3179a05b84
Justin Handville <nanolith@gmail.com>
2026-01-16 15:18:52 -0500

    Stub for libsat header.

---
+++ include/libsat/libsat.h
@@ -0,0 +1,20 @@
+/**
+ * \file libsat/libsat.h
+ *
+ * \brief Main header for libsat.
+ *
+ * \copyright 2025-2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
