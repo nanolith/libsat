3996788900bc0b7b5c9bb3b2dc55886fd5eb3bd1
Justin Handville <nanolith@gmail.com>
2026-01-16 16:19:41 -0500

    libsat_scanner decl.

---
+++ include/libsat/scanner.h
@@ -0,0 +1,35 @@
+/**
+ * \file libsat/scanner.h
+ *
+ * \brief Scanner for libsat.
+ *
+ * \copyright 2025-2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <rcpr/resource.h>
+#include <rcpr/resource/protected.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/**
+ * \brief A scanner capable of generating tokens based on input strings for
+ * creating propositional logic statements.
+ */
+typedef struct libsat_scanner libsat_scanner;
+struct libsat_scanner
+{
+    RCPR_SYM(resource) hdr;
+    const char* input;
+    size_t index;
+};
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
