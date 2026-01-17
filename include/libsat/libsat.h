b89027e8efa6c0ca9a35b1988b3313da8344fdc7
Justin Handville <nanolith@gmail.com>
2026-01-16 19:40:14 -0500

    libsat_context impl.

--- include/libsat/libsat.h
+++ include/libsat/libsat.h
@@ -10,11 +10,27 @@
 #pragma once
 
 #include <libsat/scanner.h>
+#include <rcpr/allocator.h>
+#include <rcpr/rbtree.h>
+#include <rcpr/resource.h>
+#include <rcpr/resource/protected.h>
 
 /* C++ compatibility. */
 # ifdef   __cplusplus
 extern "C" {
 # endif /*__cplusplus*/
+
+/**
+ * \brief Context for the libsat instance.
+ */
+typedef struct LIBSAT_SYM(libsat_context) LIBSAT_SYM(libsat_context);
+struct LIBSAT_SYM(libsat_context)
+{
+    RCPR_SYM(resource) hdr;
+    RCPR_SYM(allocator)* alloc;
+    RCPR_SYM(rbtree)* intern;
+    size_t variable_count;
+};
 
 /* C++ compatibility. */
 # ifdef   __cplusplus
