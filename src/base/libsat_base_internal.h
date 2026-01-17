ca713c73bee0656f0c31e23bf7ff8e424d55d13e
Justin Handville <nanolith@gmail.com>
2026-01-17 12:23:48 -0500

    string_to_intern_tree_compare decl.

--- src/base/libsat_base_internal.h
+++ src/base/libsat_base_internal.h
@@ -90,6 +90,23 @@ RCPR_SYM(rcpr_comparison_result)
 LIBSAT_SYM(intern_to_string_tree_compare)(
     void* context, const void* lhs, const void* rhs);
 
+/**
+ * \brief Compare two opaque \ref intern_entry values for sorting in an
+ * string-to-intern tree mapping.
+ *
+ * \param context       Unused.
+ * \param lhs           The left-hand side of the comparison.
+ * \param rhs           The right-hand side of the comparison.
+ *
+ * \returns an integer value representing the comparison result.
+ *      - RCPR_COMPARE_LT if \p lhs &lt; \p rhs.
+ *      - RCPR_COMPARE_EQ if \p lhs == \p rhs.
+ *      - RCPR_COMPARE_GT if \p lhs &gt; \p rhs.
+ */
+RCPR_SYM(rcpr_comparison_result)
+LIBSAT_SYM(string_to_intern_tree_compare)(
+    void* context, const void* lhs, const void* rhs);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
