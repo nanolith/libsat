fe42ba6d92895a03b8f9b728d5cdf497b3cf2b51
Justin Handville <nanolith@gmail.com>
2026-01-16 19:37:59 -0500

    Forward declarations for libsat.

---
+++ include/libsat/libsat_fwd.h
@@ -0,0 +1,27 @@
+/**
+ * \file libsat/libsat_fwd.h
+ *
+ * \brief Forward declarations for libsat.
+ *
+ * \copyright 2025-2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <libsat/function_decl.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/**
+ * \brief Forward declaration for context.
+ */
+typedef struct LIBSAT_SYM(libsat_context) LIBSAT_SYM(libsat_context);
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
