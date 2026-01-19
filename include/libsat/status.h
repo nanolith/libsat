f91e23513eae4f122944850990c6a27c1870e318
Justin Handville <nanolith@gmail.com>
2026-01-18 19:21:05 -0500

    status header.

---
+++ include/libsat/status.h
@@ -0,0 +1,14 @@
+/**
+ * \file libsat/status.h
+ *
+ * \brief Status codes for libsat.
+ *
+ * \copyright 2025-2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <libsat/component.h>
+#include <libsat/status/base.h>
+#include <rcpr/status.h>
