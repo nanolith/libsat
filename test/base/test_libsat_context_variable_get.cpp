1693299d962380dc7b2e01f7b385c95cf239ae9a
Justin Handville <nanolith@gmail.com>
2026-01-18 21:33:13 -0500

    Test that CREATE and REF flags can't be combined.

---
+++ test/base/test_libsat_context_variable_get.cpp
@@ -0,0 +1,48 @@
+/**
+ * \file base/test_libsat_context_variable_get.cpp
+ *
+ * \brief Unit tests for libsat_context_variable_get.
+ *
+ * \copyright 2025-2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <libsat/libsat.h>
+#include <libsat/status.h>
+#include <minunit/minunit.h>
+
+LIBSAT_IMPORT_base;
+RCPR_IMPORT_allocator;
+RCPR_IMPORT_resource;
+
+TEST_SUITE(libsat_context_variable_get);
+
+/**
+ * CREATE and REF can't be used in the same variable get call.
+ */
+TEST(CREATE_REF)
+{
+    allocator* alloc;
+    libsat_context* context;
+    size_t var_id = 0;
+
+    /* create malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* create context. */
+    TEST_ASSERT(STATUS_SUCCESS == libsat_context_create(&context, alloc));
+
+    /* CREATE and REF cannot be combined in flags. */
+    TEST_ASSERT(
+        ERROR_LIBSAT_BASE_VARIABLE_GET_INCOMPATIBLE_FLAGS
+            == libsat_context_variable_get(
+                    &var_id, context, "x",
+                    LIBSAT_VARIABLE_GET_CREATE | LIBSAT_VARIABLE_GET_REF));
+
+    /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == resource_release(libsat_context_resource_handle(context)));
+    TEST_ASSERT(
+        STATUS_SUCCESS == resource_release(allocator_resource_handle(alloc)));
+}
