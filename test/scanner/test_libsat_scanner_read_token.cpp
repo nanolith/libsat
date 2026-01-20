77597a9b2560e6085cc3fc49546c9d1781d247fe
Justin Handville <nanolith@gmail.com>
2026-01-19 19:45:09 -0500

    Test that we can scan a false literal.

--- test/scanner/test_libsat_scanner_read_token.cpp
+++ test/scanner/test_libsat_scanner_read_token.cpp
@@ -377,3 +377,56 @@ TEST(true_literal)
     TEST_ASSERT(
         STATUS_SUCCESS == resource_release(allocator_resource_handle(alloc)));
 }
+
+/**
+ * We can scan a false literal.
+ */
+TEST(false_literal)
+{
+    allocator* alloc;
+    libsat_context* context;
+    libsat_scanner* scanner;
+    libsat_scanner_token details;
+    const char* input = R"( false )";
+
+    /* create malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* create context. */
+    TEST_ASSERT(STATUS_SUCCESS == libsat_context_create(&context, alloc));
+
+    /* create scanner. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == libsat_scanner_create(&scanner, context, input));
+
+    /* read a token from the scanner. */
+    int token = libsat_scanner_read_token(&details, scanner);
+
+    /* this token should be LITERAL_FALSE. */
+    TEST_EXPECT(LIBSAT_SCANNER_TOKEN_TYPE_LITERAL_FALSE == token);
+
+    /* verify the details. */
+    TEST_EXPECT(token == details.type);
+    TEST_EXPECT(1 == details.begin_index);
+    TEST_EXPECT(5 == details.end_index);
+    TEST_EXPECT(1 == details.begin_line);
+    TEST_EXPECT(1 == details.end_line);
+    TEST_EXPECT(2 == details.begin_col);
+    TEST_EXPECT(6 == details.end_col);
+
+    /* read a token from the scanner. */
+    token = libsat_scanner_read_token(&details, scanner);
+
+    /* this token should be EOF. */
+    TEST_EXPECT(LIBSAT_SCANNER_TOKEN_TYPE_EOF == token);
+
+    /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            resource_release(libsat_scanner_resource_handle(scanner)));
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            resource_release(libsat_context_resource_handle(context)));
+    TEST_ASSERT(
+        STATUS_SUCCESS == resource_release(allocator_resource_handle(alloc)));
+}
