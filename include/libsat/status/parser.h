dc7e923cc2512968ab4a4521040c653d0f814fc3
Justin Handville <nanolith@gmail.com>
2026-01-20 19:38:59 -0500

    ERROR_LIBSAT_PARSER_EMPTY_INPUT decl.

---
+++ include/libsat/status/parser.h
@@ -0,0 +1,18 @@
+/**
+ * \file libsat/status/parser.h
+ *
+ * \brief parser status codes for libsat.
+ *
+ * \copyright 2025-2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <libsat/status.h>
+
+/**
+ * \brief Empty input was encountered.
+ */
+#define ERROR_LIBSAT_PARSER_EMPTY_INPUT \
+    STATUS_CODE(1, LIBSAT_COMPONENT_PARSER, 0x0000)
