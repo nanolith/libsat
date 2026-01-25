e9c63cfeb71a3546377ab335531fe640f5569b03
Justin Handville <nanolith@gmail.com>
2026-01-24 19:56:19 -0500

    libsat_ast_node_create_as_negation export.

--- src/parser/parser_internal.h
+++ src/parser/parser_internal.h
@@ -104,6 +104,11 @@ LIBSAT_SYM(libsat_ast_node_resource_release)(
                 LIBSAT_SYM(libsat_ast_node_create_from_boolean_literal)( \
                     x,y,z); } \
     static inline status FN_DECL_MUST_CHECK \
+    sym ## libsat_ast_node_create_as_negation( \
+        RCPR_SYM(libsat_ast_node)** x, LIBSAT_SYM(libsat_context)* y \
+        LIBSAT_SYM(libsat_ast_node)* z) { \
+            return LIBSAT_SYM(libsat_ast_node_create_as_negation)(x,y,z); } \
+    static inline status FN_DECL_MUST_CHECK \
     sym ## libsat_ast_node_resource_release( \
         RCPR_SYM(resource)* x) { \
             return LIBSAT_SYM(libsat_ast_node_resource_release)(x); } \
