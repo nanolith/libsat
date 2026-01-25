ffb0b72bdfdbe4f3233e9452982aaeccf50640d2
Justin Handville <nanolith@gmail.com>
2026-01-24 20:07:27 -0500

    libsat_ast_node_create_as_implication export.

--- src/parser/parser_internal.h
+++ src/parser/parser_internal.h
@@ -212,6 +212,12 @@ LIBSAT_SYM(libsat_ast_node_resource_release)(
                 LIBSAT_SYM(libsat_ast_node_create_as_exclusive_disjunction)( \
                     w,x,y,z); } \
     static inline status FN_DECL_MUST_CHECK \
+    sym ## libsat_ast_node_create_as_implication( \
+        LIBSAT_SYM(libsat_ast_node)** w, LIBSAT_SYM(libsat_context)* x, \
+        LIBSAT_SYM(libsat_ast_node)* y, LIBSAT_SYM(libsat_ast_node)* z) { \
+            return \
+                LIBSAT_SYM(libsat_ast_node_create_as_implication)(w,x,y,z); } \
+    static inline status FN_DECL_MUST_CHECK \
     sym ## libsat_ast_node_resource_release( \
         RCPR_SYM(resource)* x) { \
             return LIBSAT_SYM(libsat_ast_node_resource_release)(x); } \
