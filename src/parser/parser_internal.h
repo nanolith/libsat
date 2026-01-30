18d9d3e11dc7abe584261dadad2399a7c14e57c8
Justin Handville <nanolith@gmail.com>
2026-01-30 03:09:56 -0500

    LIBSAT_OPERATOR_PRIORITY_DISJUNCITON decl.

--- src/parser/parser_internal.h
+++ src/parser/parser_internal.h
@@ -22,6 +22,7 @@ enum LIBSAT_SYM(libsat_operator_priority)
     LIBSAT_OPERATOR_PRIORITY_NEGATION                                   = 1,
     LIBSAT_OPERATOR_PRIORITY_CONJUNCTION                                = 2,
     LIBSAT_OPERATOR_PRIORITY_EXCLUSIVE_DISJUNCITON                      = 3,
+    LIBSAT_OPERATOR_PRIORITY_DISJUNCITON                                = 4,
 };
 
 /******************************************************************************/
