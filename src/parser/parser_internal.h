f524281bf85bb4a2f67d82880aef84e66f8a930c
Justin Handville <nanolith@gmail.com>
2026-01-30 03:12:01 -0500

    LIBSAT_OPERATOR_PRIORITY_PARENTHESES decl.

--- src/parser/parser_internal.h
+++ src/parser/parser_internal.h
@@ -19,6 +19,7 @@ extern "C" {
 
 enum LIBSAT_SYM(libsat_operator_priority)
 {
+    LIBSAT_OPERATOR_PRIORITY_PARENTHESES                                = 0,
     LIBSAT_OPERATOR_PRIORITY_NEGATION                                   = 1,
     LIBSAT_OPERATOR_PRIORITY_CONJUNCTION                                = 2,
     LIBSAT_OPERATOR_PRIORITY_EXCLUSIVE_DISJUNCITON                      = 3,
