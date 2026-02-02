95fc1c73f9c1ecff6152bacbe142516f0f00a5a2
Justin Handville <nanolith@gmail.com>
2026-02-02 03:05:18 -0500

    Removed assignment constructor export.

--- src/parser/parser_internal.h
+++ src/parser/parser_internal.h
@@ -354,13 +354,6 @@ bool LIBSAT_SYM(should_combine_left)(int left, int right);
                 LIBSAT_SYM(libsat_ast_node_create_as_biconditional)( \
                     w,x,y,z); } \
     static inline status FN_DECL_MUST_CHECK \
-    sym ## libsat_ast_node_create_as_assignment( \
-        LIBSAT_SYM(libsat_ast_node)** w, LIBSAT_SYM(libsat_context)* x, \
-        LIBSAT_SYM(libsat_ast_node)* y, LIBSAT_SYM(libsat_ast_node)* z) { \
-            return \
-                LIBSAT_SYM(libsat_ast_node_create_as_assignment)( \
-                    w,x,y,z); } \
-    static inline status FN_DECL_MUST_CHECK \
     sym ## libsat_ast_node_create_as_statement( \
         LIBSAT_SYM(libsat_ast_node)** x, LIBSAT_SYM(libsat_context)* y, \
         LIBSAT_SYM(libsat_ast_node)* z) { \
